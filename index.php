5fa0ed955f726b13f94da755487dc56c1121fa02
videost <137654194+videost@users.noreply.github.com>
2023-06-24 16:16:22 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,40 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+/*
+Plugin Name: Ultimate Video Player
+Plugin URI: http://codecanyon.net/user/FWDesign/portfolio
+Description: This is the WordPress plugin with a CMS menu for the installation and configuration of the Ultimate Video Player.
+Text Domain: fwduvp
+Author: FWDesign
+Version: 9.0
+License: GPLv2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+include_once "php/FWDUVP.php";
+include_once "php/FWDUVPData.php";
+define('FWDUVP_TEXT_DOMAIN',  wp_get_theme()->get('TextDomain'));
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+
+function fwduvp_check_if_admin(){
+	$roles = wp_get_current_user()->roles;
+	$role = "administrator";
+	 
+	return in_array($role, $roles);
+}
+
+function fwduvp_admin_init(){
+	if (fwduvp_check_if_admin()){
+		$role = get_role("administrator");
+		$role->add_cap(FWDUVP::CAPABILITY);
+	}
+}
+
+function fwduvp_init_plugin(){	
+	$uvp = new FWDUVP();
+	$uvp->init();
+}
+
+add_action("init", "fwduvp_init_plugin");
+add_action("admin_init", "fwduvp_admin_init");
+add_filter("plugin_action_links_" . plugin_basename(__FILE__), array("FWDUVP", "fwduvp_set_action_links"));
+?>
